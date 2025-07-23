04ecdcc39ac893f2a643280c4e74f8534c73d194
Mirco <mirco@MacBook-Pro-von-Mirco.local>
2025-07-23 17:13:14 +0200

    add theme toggle logic

--- app/components/theme-toggle.vue
+++ app/components/theme-toggle.vue
@@ -1,15 +1,26 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
 <template>
   <label class="swap swap-rotate mx-4">
     <!-- this hidden checkbox controls the state -->
-    <input type="checkbox">
-
+    <input v-model="isDark" type="checkbox">
     <!-- sun icon -->
     <Icon
       class="swap-on"
       name="tabler:sun"
       size="24"
     />
-
     <!-- moon icon -->
     <Icon
       class="swap-off"
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,7 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  colorMode: {
+    dataValue: "theme",
+  },
 });
