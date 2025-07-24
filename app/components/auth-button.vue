45807be4dfddc2738ab699055022e13d58e2d1eb
Mirco <mirco@MacBook-Pro-von-Mirco.local>
2025-07-24 14:32:06 +0200

    add loading spinner

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -2,18 +2,30 @@
 import { createAuthClient } from "better-auth/client";
 
 const authClient = createAuthClient();
+const loading = ref(false);
 
 async function signIn() {
+  loading.value = true;
   await authClient.signIn.social({
     provider: "github",
     callbackURL: "/dashboard",
   });
+  loading.value = false;
 }
 </script>
 
 <template>
-  <button class="btn btn-accent" @click="signIn">
+  <button
+    :disabled="loading"
+    class="btn btn-accent"
+    @click="signIn"
+  >
     Sign In With Github
-    <Icon name="tabler:brand-github" size="24" />
+    <span v-if="loading" class="loading loading-soinner loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
   </button>
 </template>
